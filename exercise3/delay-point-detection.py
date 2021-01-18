105ac53a2b31b31d9ab69a627547a6497623d041
a1da4 <ai.the.taichi@gmail.com>
2021-01-19 01:03:50 +0900

    load dataset, initialize delay matrix

--- exercise3/delay-point-detection.py
+++ exercise3/delay-point-detection.py
@@ -1,8 +1,8 @@
 # 巨大な遅延時間を発生しているノードを発見する
-from pprint import pprint
-
 import numpy as np
 
+# ネットワークの設定
+print("\n1. define network...")
 N = 21
 hops = [
     [0, 4, 4, 4, 4, 5, 5, 4, 5, 5, 4, 4, 2, 1, 3, 3, 4, 4, 3, 2, 3],
@@ -27,7 +27,6 @@ hops = [
     [2, 2, 2, 2, 2, 3, 3, 2, 3, 3, 2, 2, 2, 1, 1, 1, 2, 2, 1, 0, 1],
     [3, 3, 3, 3, 3, 2, 2, 1, 2, 2, 3, 3, 3, 2, 2, 2, 1, 1, 2, 1, 0],
 ]
-print(f"{len(hops)} times {len(hops[0])}")
 for i in range(N):
     for j in range(N):
         assert hops[i][j] == hops[j][i], f"DistanceDoesNotMatch {i}, {j}"
@@ -44,9 +43,54 @@ routes = [
     [7, 20, 19, 14, 2],
     [8, 17, 20, 19, 15, 4],
 ]
-R = np.zeros([len(routes), N])
+A = np.zeros([len(routes), N])
 for i, route in enumerate(routes):
     for node in route:
-        R[i][node] += 1
-pprint(f"routes: {routes}")
-print(R)
+        A[i][node] += 1
+print(f" - routes: {routes}")
+print(f" - A: {A}")
+print("done")
+
+# データの読み込み
+print("\n2. load delay dataset...")
+y = []
+with open("delay.csv") as fp:
+    for line in fp:
+        delays = "".join(line.strip().split())[:-1]
+        delays = delays.split(",")
+        delays = [float(delay) for delay in delays]
+        y.append(delays)
+        #print(delays)
+        #print(len(delays))
+y = np.array(y)
+print(f" - y: {y.shape}")
+print("done")
+
+# 各ノードの遅延時間 X の初期値は、ノードの hop に応じて決めた方が良さそう
+print("\n3. initialize delay matrix...")
+def calc_cov(i, j, var, hops, delta):
+    if i == j:
+        return var
+    else:
+        var *= np.exp(-hops[i][j] / delta)
+        return var
+
+# データを元に平均を出したい
+#mu = np.average(A)
+mu = np.average(y)
+print(f" - mu: {mu}")
+c = 2.0
+sigma = c * mu
+delta = 5.0
+cov = np.zeros([N, N])
+
+for i in range(N):
+    for j in range(N):
+        var = calc_cov(i, j, sigma ** 2, hops, delta)
+        cov[i][j] = var
+
+#print(cov)
+mean = np.array([mu for _ in range(N)])
+X = np.random.multivariate_normal(mean, cov)
+print(f" - X: {X}")
+print("done")
